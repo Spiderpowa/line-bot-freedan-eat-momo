6b3d6051548909c2d694bfad1e0de4719d5c307d
Jimmy Hu <jim2212001@gmail.com>
2018-02-13 16:54:44 +0800

    typo source

--- bot.py
+++ bot.py
@@ -37,7 +37,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    print(event.soruce)
+    print(event.source)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=str(event.source)))
