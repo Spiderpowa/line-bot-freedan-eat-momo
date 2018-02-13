3e0d5d3dc7c20e25cf6123ea0579349829b4b564
Jimmy Hu <jim2212001@gmail.com>
2018-02-14 00:39:02 +0800

    modify handler

--- bot.py
+++ bot.py
@@ -15,32 +15,35 @@ class MoMoBot:
     def __init__(self, line_bot_api, handler):
         self.app = Flask(__name__)
         self.line_bot_api = line_bot_api
-        self.handler = handler
+        self.handler = None
         
         self.app.add_url_rule("/callback", None, self.__callback, methods=['POST'])
 
-        @self.handler.add(MessageEvent, message=TextMessage)
-        def handle_message(self, event):
-            print(event)
-            if '吃MoMo' in event.message.text:
+    def set_handler(self, handler):
+        self.handler = handler
+
+    
+    def handle_message(self, event):
+        print(event)
+        if '吃MoMo' in event.message.text:
+            self.line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text='京站MoMo訂位專線:(02)2550-0889'))
+        if event.source.user_id is None:
+            return
+        if event.source.type == 'user':
+            profile = line_bot_api.get_profile(event.source.user_id)
+        elif event.source.type == 'group':
+            profile = line_bot_api.get_group_member_profile(
+                event.source.group_id, event.source.user_id)
+        print(profile)
+        freedan = ['FreeDan', '弗力丹', '阿丹']
+        for name in freedan:
+            if name in profile.display_name:
                 self.line_bot_api.reply_message(
                     event.reply_token,
-                    TextSendMessage(text='京站MoMo訂位專線:(02)2550-0889'))
-            if event.source.user_id is None:
+                    TextSendMessage(text='阿丹吃MoMo阿'))
                 return
-            if event.source.type == 'user':
-                profile = line_bot_api.get_profile(event.source.user_id)
-            elif event.source.type == 'group':
-                profile = line_bot_api.get_group_member_profile(
-                    event.source.group_id, event.source.user_id)
-            print(profile)
-            freedan = ['FreeDan', '弗力丹', '阿丹']
-            for name in freedan:
-                if name in profile.display_name:
-                    self.line_bot_api.reply_message(
-                        event.reply_token,
-                        TextSendMessage(text='阿丹吃MoMo阿'))
-                    return
 
     def __callback(self):
         # get X-Line-Signature header value
@@ -49,6 +52,10 @@ class MoMoBot:
         # get request body as text
         body = request.get_data(as_text=True)
         self.app.logger.info("Request body: " + body)
+
+
+        if self.handler is None:
+            abort(202) # Accept
 
         # handle webhook body
         try:
@@ -67,6 +74,14 @@ class MoMoBot:
 
 if __name__ == "__main__":
     line_bot_api = LineBotApi(os.environ['LINEBOT_CHANNEL_ACCESS_TOKEN'])
-    handler = WebhookHandler(os.environ['LINEBOT_CHANNEL_SECRET'])
+
     bot = MoMoBot(line_bot_api, handler)
+
+    handler = WebhookHandler(os.environ['LINEBOT_CHANNEL_SECRET'])
+    @handler.add(MessageEvent, message=TextMessage)
+    def handle_message(event):
+        bot.handle_message(event)
+
+    bot.set_handler(handler)
     bot.run()
+
--- main.py
+++ main.py
@@ -5,5 +5,12 @@ from linebot import (
 import os
 
 line_bot_api = LineBotApi(os.environ['LINEBOT_CHANNEL_ACCESS_TOKEN'])
-handler = WebhookHandler(os.environ['LINEBOT_CHANNEL_SECRET'])
+
 app = MoMoBot(line_bot_api, handler)
+
+handler = WebhookHandler(os.environ['LINEBOT_CHANNEL_SECRET'])
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    app.handle_message(event)
+
+app.set_handler(handler)
