5e53ab364613877520800e5244129eed5a5d484b
Jimmy Hu <jim2212001@gmail.com>
2018-02-14 00:19:03 +0800

    The run is not needed

--- main.py
+++ main.py
@@ -7,4 +7,3 @@ import os
 line_bot_api = LineBotApi(os.environ['LINEBOT_CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['LINEBOT_CHANNEL_SECRET'])
 app = MoMoBot(line_bot_api, handler)
-app.run()
