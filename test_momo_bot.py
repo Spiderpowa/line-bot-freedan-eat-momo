1a5b13372d2c2d2a5c390fe9fd606343c5f2f735
Jimmy Hu <jim2212001@gmail.com>
2018-02-18 00:19:37 +0800

    Use pytest to test current functions

--- test_momo_bot.py
+++ test_momo_bot.py
@@ -1,36 +1,92 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, request, abort
-import os
-
 from linebot import (
-    LineBotApi, WebhookHandler
+    LineBotApi
 )
-from linebot.exceptions import (
-    InvalidSignatureError
-)
+
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextSendMessage
 )
 
-import random, string
+from linebot.models.sources import (
+    SourceUser, SourceGroup
+)
 
-import FakeServer
+from linebot.models.messages import (
+    TextMessage
+)
+
+from linebot.models.responses import (
+    Profile
+)
+
 from bot import MoMoBot
+from unittest.mock import MagicMock
+import pytest
 
-random.seed(25500889)
-LINEBOT_CHANNEL_ACCESS_TOKEN = ''.join(random.choices(string.hexdigits, k=25))
-LINEBOT_CHANNEL_SECRET = ''.join(random.choices(string.hexdigits, k=25))
+class TestMoMoBot:
+    @pytest.fixture(scope='class')
+    def line_bot_api(self):
+        return LineBotApi('access_token')
 
-def test_myself():
-    client = FakeServer.HttpClient()
-    line_bot_api = LineBotApi(LINEBOT_CHANNEL_ACCESS_TOKEN, http_client = client)
+    @pytest.fixture(scope='class')
+    def bot(self, request, line_bot_api):
+        return MoMoBot(line_bot_api)
 
-    bot = MoMoBot(line_bot_api)
+    def test_momo_phone(self, bot, line_bot_api):
+        profile = Profile(display_name="RandomGuy")
 
-    handler = WebhookHandler(LINEBOT_CHANNEL_SECRET)
-    @handler.add(MessageEvent, message=TextMessage)
-    def handle_message(event):
+        line_bot_api.get_profile = MagicMock(return_value=profile)
+        line_bot_api.get_group_member_profile = MagicMock(return_value=profile)
+
+        source = SourceUser("Uxxxxxxxxx")
+        message = TextMessage()
+        event = MessageEvent(source=source, message=message)
+        
+        # Test '吃MoMo' case insensitive
+        line_bot_api.reply_message = MagicMock()
+        message.text = "吃MoMo"
+        bot.handle_message(event)
+        message.text = "吃momo"
+        bot.handle_message(event)
+        message.text = "來吃mOmO"
+        bot.handle_message(event)
+        message.text = "想吃MOMO阿"
         bot.handle_message(event)
 
-    bot.set_handler(handler)
+        assert line_bot_api.reply_message.call_count == 4
+        for (reply, _) in line_bot_api.reply_message.call_args_list:
+            assert reply[1].text == '京站MoMo訂位專線:(02)2550-0889'
+
+        # Test normal message
+        line_bot_api.reply_message.reset_mock()
+        message.text = "沒有要吃喔"
+        bot.handle_message(event)
+        assert line_bot_api.reply_message.call_count == 0
+    
+    def test_freedan(self, bot, line_bot_api):
+        source = SourceUser("Uxxxxxxxxx")
+        event = MessageEvent(source=source, message=TextMessage(text="RandomMessage"))
+        line_bot_api.reply_message = MagicMock()
+
+        profile = Profile()
+        line_bot_api.get_profile = MagicMock(return_value=profile)
+
+        # Test 'FreeDan' and his possible name
+        profile.display_name = 'FreeDan - 啊哈'
+        bot.handle_message(event)
+        profile.display_name = '數學教師弗力丹'
+        bot.handle_message(event)
+        profile.display_name = '阿丹不是阿舟'
+        bot.handle_message(event)
+        
+        assert line_bot_api.reply_message.call_count == 3
+        for (reply, _) in line_bot_api.reply_message.call_args_list:
+            assert reply[1].text == '阿丹吃MoMo阿'
+
+        # Test normal people
+        line_bot_api.reply_message.reset_mock()
+        profile.display_name = '冰凍麵包'
+        bot.handle_message(event)
+        assert line_bot_api.reply_message.call_count == 0
+
